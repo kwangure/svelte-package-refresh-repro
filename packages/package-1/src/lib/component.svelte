74f74c09c31a83e40cb3806444ce3bcb8e81c59e
kwangure <kaf.wangure@gmail.com>
2023-08-09 15:06:54 +0300

    feat: initial commit

---
+++ packages/package-1/src/lib/component.svelte
@@ -0,0 +1,2 @@
+Helloow world!
+<!-- Monkeys have toes -->
---
+++ packages/site-1/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { Component } from '@monorepo/package-1'
+</script>
+
+<Component />
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
